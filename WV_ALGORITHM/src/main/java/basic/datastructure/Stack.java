eb50be2182211ad1259f4757c8db050307b7dbb4
kmidev77 <leejy@kmi.or.kr>
2023-03-29 16:57:50 +0900

    0328_data structure

--- WV_ALGORITHM/src/main/java/basic/datastructure/Stack.java
+++ WV_ALGORITHM/src/main/java/basic/datastructure/Stack.java
@@ -1,4 +1,70 @@
 package basic.datastructure;
 
+import java.util.Scanner;
+
 public class Stack {
+
+    /*
+    Stack은 한 쪽 끝에서만 자료를 넣거나 뺄 수 있는 선형 구조(LIFO - Last In First Out)로 데이터를 저장하는 형식을 말한다.
+     */
+
+    int top = 0; // 클래스변수
+    int[] stackArray = new int[2]; // 배열
+    final int MAX_N = 3;
+
+    public static void main(String[] args) throws Exception {
+
+        Scanner sc = new Scanner(System.in);
+
+    }
+
+    // 스택 초기화
+    public void stackInit() {
+        top = 0;
+    }
+
+    //
+    public boolean stackIsEmpty() {
+        if (top == 0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    //
+    public boolean stackIsFull() {
+        if (top == MAX_N) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    // stack에 값 넣기
+    public boolean stackPush(int value) {
+
+        if (stackIsFull()) {
+            System.out.println("[Stack] stack overflow");
+            return false;
+        }
+
+        stackArray[top] = value; // value값을 top번째의 값으로 넣고
+        top ++; // stack top 1증가
+
+        return true;
+    }
+
+    public boolean stackPop() {
+
+        if (stackIsEmpty()) {
+            System.out.println("[Stack] stack is empty");
+        }
+
+        top --;
+
+        return true;
+    }
+
+
 }
---
+++ WV_ALGORITHM/src/main/java/basic/j0001/RecommandBus.java
@@ -0,0 +1,17 @@
+package basic.j0001;
+
+import java.util.Scanner;
+
+// https://jinhongstar.tistory.com/76
+public class RecommandBus {
+    // 현재 시각과 이후 시각을 비교하여 현재가 과거일 경우 제낌 ==> 모두 제껴졌을 경우 가장 이른 시각을 선택
+    // 남은 것들 중에 가장 시간차가 적은 것을 선택
+    //   ㄴ 시간차:번째 를 저장해서 시간차가 가장 작은 것만 저장 후 리턴
+
+    public static Scanner scanner = new Scanner(System.in);
+
+    public void functionA() {
+        String input = scanner.next();
+        String[] splited = input.split(" ");
+    }
+}
